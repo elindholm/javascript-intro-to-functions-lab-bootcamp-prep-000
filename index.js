28d13e564d9610c0ffa1e96af5d3f0b39cf57ac7
Erica Lindholm <elindholm94@gmail.com>
2019-08-13 02:15:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   return ("I can't hear you!")
  if (string)toLowerCase
-
+}
    return ("YES INDEED!")
   if (string)toUpperCase
-
+}
   return ("I love you, too")
  if (string)"I love you, Gandma"
 }
